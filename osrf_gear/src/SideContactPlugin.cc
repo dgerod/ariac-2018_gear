c4830d0996fa7d2bafbe152e2a3635b379e6b25a
dhood <dhood@users.noreply.github.com>
2017-06-07 09:54:42 -0700

    Place 'cleared' tray parts next to each other on an under-workcell plane
    Instead of falling towards -inf

--- osrf_gear/src/SideContactPlugin.cc
+++ osrf_gear/src/SideContactPlugin.cc
@@ -206,11 +206,14 @@ void SideContactPlugin::CalculateContactingModels()
 void SideContactPlugin::ClearContactingModels()
 {
   boost::mutex::scoped_lock lock(this->mutex);
+  int index = 0;
   for (auto model : this->contactingModels)
   {
     gzdbg << "Teleporting model: " << model->GetScopedName() << std::endl;
+    model->SetAutoDisable(true);
     model->SetGravityMode(true);
-    model->SetWorldPose(math::Pose(0, 0, -1, 0, 0, 0));
+    model->SetStatic(true);
+    model->SetWorldPose(math::Pose(0 + 0.25*index++, 0, -1, 0, 0, 0));
   }
 }
 
