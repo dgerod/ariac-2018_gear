31268639cdf2def230be71dff37ae0eecee0ac33
dhood <dhood@users.noreply.github.com>
2018-01-23 17:35:22 -0800

    Allow belt control in comp mode

--- osrf_gear/src/ROSConveyorBeltPlugin.cc
+++ osrf_gear/src/ROSConveyorBeltPlugin.cc
@@ -77,16 +77,6 @@ bool ROSConveyorBeltPlugin::OnControlCommand(ros::ServiceEvent<
   const std::string& callerName = event.getCallerName();
   gzdbg << "Conveyor control service called by: " << callerName << std::endl;
 
-  // During the competition, this environment variable will be set.
-  auto compRunning = std::getenv("ARIAC_COMPETITION");
-  if (compRunning && callerName.compare("/gazebo") != 0)
-  {
-    std::string errStr = "Competition is running so this service is not enabled.";
-    gzerr << errStr << std::endl;
-    ROS_ERROR_STREAM(errStr);
-    res.success = false;
-    return true;
-  }
   if (this->IsEnabled())
   {
     this->SetPower(req.state.power);
