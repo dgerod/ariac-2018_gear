4ce540b8b82e1de8f2fc5e3dc8e8fb7e8262c8e4
dhood <dhood@users.noreply.github.com>
2018-03-21 18:39:37 -0700

    Version 2.1.0

--- osrf_gear/src/ROSAriacTaskManagerPlugin.cc
+++ osrf_gear/src/ROSAriacTaskManagerPlugin.cc
@@ -199,7 +199,7 @@ ROSAriacTaskManagerPlugin::~ROSAriacTaskManagerPlugin()
 void ROSAriacTaskManagerPlugin::Load(physics::WorldPtr _world,
   sdf::ElementPtr _sdf)
 {
-  gzdbg << "ARIAC VERSION: 2.0.6\n";
+  gzdbg << "ARIAC VERSION: 2.1.0\n";
   auto competitionEnv = std::getenv("ARIAC_COMPETITION");
   this->dataPtr->competitonMode = competitionEnv != NULL;
   gzdbg << "ARIAC COMPETITION MODE: " << (this->dataPtr->competitonMode ? competitionEnv : "false") << std::endl;
