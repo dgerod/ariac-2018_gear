31717e598c658a1e901f683246359f7a1fca0579
dhood <dhood@users.noreply.github.com>
2018-03-31 03:44:29 +1000

    Check tf frames from logical cam aren't published

--- test_ariac/test_sensor_blackout.py
+++ test_ariac/test_sensor_blackout.py
@@ -26,18 +26,22 @@ class SensorBlackoutTester(TfTester):
 
     def test(self):
         self.prepare_tester()
-
         self.prepare_tf()
 
-        # Starting the competition will cause the sensor blackout period to be triggered
+        # Starting the competition will cause the sensor blackout period to be triggered.
         self._test_start_comp()
         rospy.sleep(2.5)
 
+        # The TF frames from the logical cameras should not be published.
+        self._test_no_tf_frames()
+
+        # Subscribe to sensors.
+        # No messages should be received during the blackout period.
+        # This requires a change to the default behaviour of gazebo_plugins to not re-enable the
+        # sensors when subscribers connect.
         self.subscribe_to_sensors()
         start_time = rospy.Time.now()
-        rospy.loginfo("sleeping")
 	while (rospy.Time.now() - start_time).to_sec() < 5:
-            rospy.loginfo("sleeping")
             rospy.sleep(0.1)
 
     def _callback(self, msg):
@@ -56,38 +60,10 @@ class SensorBlackoutTester(TfTester):
             "/ariac/laser_profiler_1", LaserScan, self._callback)
         break_beam_sub = rospy.Subscriber(
             "/ariac/break_beam_1", Proximity, self._callback)
-	'''
-        order_sub = rospy.Subscriber("/ariac/orders", Order, comp_class.order_callback)
-        joint_state_sub = rospy.Subscriber(
-            "/ariac/joint_states", JointState, comp_class.joint_state_callback)
-        gripper_state_sub = rospy.Subscriber(
-            "/ariac/gripper/state", VacuumGripperState, comp_class.gripper_state_callback)
 
-    def _test_faulty_products(self):
-        quality_control_sensor = 'quality_control_sensor_1'
-        # This product is faulty and should be reported as such in an anonymized way.
-        self._test_pose(
-            [0.1, -0.2, 0.0], tf.transformations.quaternion_from_euler(0, 0, 0),
-            quality_control_sensor + '_model_1_frame',
-            self.camera_above_box0 + '_shipping_box_0_frame'
-        )
-
-        # The model type should not be used in the name anymore (now anonymized).
+    def _test_no_tf_frames(self):
         with self.assertRaises(tf2.LookupException):
-            self._test_pose(
-                [0.1, -0.2, 0.0], tf.transformations.quaternion_from_euler(0, 0, 0),
-                quality_control_sensor + '_piston_rod_part_1_frame',
-                self.camera_above_box0 + '_shipping_box_0_frame'
-            )
-
-        # This product is not faulty and should not be found by TF.
-        with self.assertRaises(tf2.LookupException):
-            self._test_pose(
-                [0.1, -0.2, 0.0], tf.transformations.quaternion_from_euler(0, 0, 0),
-                quality_control_sensor + '_model_3_frame',
-                self.camera_above_box0 + '_shipping_box_0_frame'
-            )
-	'''
+            self._test_logical_camera_products()
 
 
 if __name__ == '__main__':
